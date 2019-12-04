1167ee9488cd954ebbe2f42faabdf983f2c2c380
Carlos Fdez. Llamas <hello@sirikon.me>
2019-12-04 23:11:53 +0100

    Tweaks.

--- scripts/update-repos.sh
+++ scripts/update-repos.sh
@@ -2,5 +2,18 @@
 
 set -e
 cd $(dirname ${BASH_SOURCE[0]})/..
+ROOT=$(pwd)
+
+echo "Pulling root repository..."
 git pull
+echo ""
+
+echo "Pulling birthdays-calendar repository..."
+cd services/birthdays-calendar
+git pull
+echo ""
+
+echo "Update submodules..."
+cd $ROOT
 git submodule update --init --recursive
+echo ""
